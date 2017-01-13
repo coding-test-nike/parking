95f9ad9d82bdc8f7798c78140a0c28a89f6bd1a3
neville <neville.lewis@gmail.com>
2017-01-13 11:51:51 -0600

    added swagger config

--- parking/parking-service/App_Start/Startup.MobileApp.cs
+++ parking/parking-service/App_Start/Startup.MobileApp.cs
@@ -4,6 +4,9 @@ using Microsoft.Azure.Mobile.Server;
 using Owin;
 using parking_service.Framework;
 using System.Web.Http.ExceptionHandling;
+using Swashbuckle.Application;
+using System.Web.Http.Description;
+using Microsoft.Azure.Mobile.Server.Swagger;
 
 namespace parking_service
 {
@@ -24,11 +27,34 @@ namespace parking_service
                      routeTemplate: "api/{controller}/{id}",
                      defaults: new { id = RouteParameter.Optional });
  
-            MobileAppSettingsDictionary settings = config.GetMobileAppSettingsProvider().GetMobileAppSettings();
+            //MobileAppSettingsDictionary settings = config.GetMobileAppSettingsProvider().GetMobileAppSettings();
 
             app.UseWebApi(config);
-            
-            
+            ConfigureSwagger(config);
+
+
+        }
+
+        public static void ConfigureSwagger(HttpConfiguration config)
+        {
+            // Use the custom ApiExplorer that applies constraints. This prevents
+            // duplicate routes on /api and /tables from showing in the Swagger doc.
+            config.Services.Replace(typeof(IApiExplorer), new MobileAppApiExplorer(config));
+            config
+               .EnableSwagger(c =>
+               {
+                   c.SingleApiVersion("v1", "Sample web api framework for the NIKE coding assignment");
+
+               // Tells the Swagger doc that any MobileAppController needs a
+               // ZUMO-API-VERSION header with default 2.0.0
+               c.OperationFilter<MobileAppHeaderFilter>();
+
+               // Looks at attributes on properties to decide whether they are readOnly.
+               // Right now, this only applies to the DatabaseGeneratedAttribute.
+               c.SchemaFilter<MobileAppSchemaFilter>();
+               
+               })
+               .EnableSwaggerUi();
         }
     }
  
