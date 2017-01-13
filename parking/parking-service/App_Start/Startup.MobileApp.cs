e5b6a75bbe168465b2df5f22ce69c8bcd73cc8fa
neville <neville.lewis@gmail.com>
2017-01-12 21:14:12 -0600

    project created and removed unnecessary things

---
+++ parking/parking-service/App_Start/Startup.MobileApp.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Http;
+using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Config;
+
+using Owin;
+
+namespace parking_service
+{
+    public partial class Startup
+    {
+        public static void ConfigureMobileApp(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.EnableCors();
+            config.MapHttpAttributeRoutes();
+
+            //for regular non-mobile app controllers
+            config.Routes.MapHttpRoute(
+                     name: "DefaultApi",
+                     routeTemplate: "api/{controller}/{id}",
+                     defaults: new { id = RouteParameter.Optional });
+
+
+            new MobileAppConfiguration()
+                .UseDefaultConfiguration()
+                .ApplyTo(config);
+
+  
+            MobileAppSettingsDictionary settings = config.GetMobileAppSettingsProvider().GetMobileAppSettings();
+
+ 
+
+            app.UseWebApi(config);
+        }
+    }
+ 
+}
+
---
+++ parking/parking-service/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(parking_service.Startup))]
+
+namespace parking_service
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
